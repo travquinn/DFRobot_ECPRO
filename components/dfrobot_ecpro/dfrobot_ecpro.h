4f0dbc4b773d874fea7e77cbaa1d862b3b5a36f9
travquinn <53845837+travquinn@users.noreply.github.com>
2024-11-10 19:18:16 +1100

    Update dfrobot_ecpro.h

--- components/dfrobot_ecpro/dfrobot_ecpro.h
+++ components/dfrobot_ecpro/dfrobot_ecpro.h
@@ -9,26 +9,30 @@ namespace dfrobot_ecpro {
 
 class DFRobotECProSensor : public sensor::Sensor, public PollingComponent {
  public:
+  DFRobotECProSensor() : PollingComponent(15000) {}
+
   void set_ads1115(ads1115::ADS1115Component *ads1115) { ads1115_ = ads1115; }
   void set_multiplexer(ads1115::ADS1115Multiplexer multiplexer) { multiplexer_ = multiplexer; }
   void set_gain(ads1115::ADS1115Gain gain) { gain_ = gain; }
+  void set_temperature(float temperature) { temperature_ = temperature; }
 
   void setup() override {
-    // Initialize the sensor
+    // Initialize the EC sensor
+    ecpro_ = new DFRobot_ECPRO();
   }
 
   void update() override {
-    // Read the sensor and publish the state
-    float voltage = ads1115_->request_measurement(multiplexer_, gain_);
-    float ec = // Convert voltage to EC value using DFRobot_ECPRO library
+    float voltage = ads1115_->request_measurement(multiplexer_, gain_, ADS1115_RESOLUTION_16_BIT);
+    float ec = ecpro_->getEC_us_cm(voltage, temperature_);
     publish_state(ec);
   }
 
  protected:
-  DFRobot_ECPRO ecpro_;
+  DFRobot_ECPRO *ecpro_;
   ads1115::ADS1115Component *ads1115_;
   ads1115::ADS1115Multiplexer multiplexer_;
   ads1115::ADS1115Gain gain_;
+  float temperature_ = 25.0; // Default temperature if not set
 };
 
 }  // namespace dfrobot_ecpro
