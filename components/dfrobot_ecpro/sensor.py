db66c6e8634a41e7358cd634c36fa756ac2a8637
travquinn <53845837+travquinn@users.noreply.github.com>
2024-11-10 19:51:45 +1100

    Update sensor.py

--- components/dfrobot_ecpro/sensor.py
+++ components/dfrobot_ecpro/sensor.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor, ads1115
-from esphome.const import CONF_ID, UNIT_MICROSIEMENS_PER_CENTIMETER, ICON_FLASH, CONF_TEMPERATURE
+from esphome.const import CONF_ID, UNIT_MICROSIEMENS_PER_CENTIMETER, ICON_FLASH
 
 DEPENDENCIES = ['ads1115']
 
@@ -9,8 +9,9 @@ dfrobot_ecpro_ns = cg.esphome_ns.namespace('dfrobot_ecpro')
 DFRobotECProSensor = dfrobot_ecpro_ns.class_('DFRobotECProSensor', sensor.Sensor, cg.PollingComponent)
 
 CONF_ADS1115_ID = 'ads1115_id'
-CONF_MULTIPLEXER = 'multiplexer'
-CONF_GAIN = 'gain'
+CONF_ADS1115_MULTIPLEXER = 'ads1115_multiplexer'
+CONF_ADS1115_GAIN = 'ads1115_gain'
+CONF_TEMPERATURE = 'temperature'
 
 CONFIG_SCHEMA = sensor.sensor_schema(
     DFRobotECProSensor,
@@ -20,8 +21,8 @@ CONFIG_SCHEMA = sensor.sensor_schema(
 ).extend({
     cv.GenerateID(): cv.declare_id(DFRobotECProSensor),
     cv.Required(CONF_ADS1115_ID): cv.use_id(ads1115.ADS1115Component),
-    cv.Required(CONF_MULTIPLEXER): cv.enum(ads1115.ADS1115_MULTIPLEXER_OPTIONS, upper=True),
-    cv.Required(CONF_GAIN): cv.enum(ads1115.ADS1115_GAIN_OPTIONS, float=True),
+    cv.Required(CONF_ADS1115_MULTIPLEXER): cv.enum(ads1115.ADS1115_MULTIPLEXER_OPTIONS, upper=True),
+    cv.Required(CONF_ADS1115_GAIN): cv.enum(ads1115.ADS1115_GAIN_OPTIONS, float=True),
     cv.Optional(CONF_TEMPERATURE, default=25.0): cv.float_,
 }).extend(cv.polling_component_schema('60s'))
 
@@ -32,6 +33,6 @@ async def to_code(config):
 
     ads = await cg.get_variable(config[CONF_ADS1115_ID])
     cg.add(var.set_ads1115(ads))
-    cg.add(var.set_multiplexer(config[CONF_MULTIPLEXER]))
-    cg.add(var.set_gain(config[CONF_GAIN]))
+    cg.add(var.set_ads1115_multiplexer(config[CONF_ADS1115_MULTIPLEXER]))
+    cg.add(var.set_ads1115_gain(config[CONF_ADS1115_GAIN]))
     cg.add(var.set_temperature(config[CONF_TEMPERATURE]))
