dd7e1ce61bf717e58544cea08dd00e9b0c0f14f7
travquinn <53845837+travquinn@users.noreply.github.com>
2024-11-10 17:48:48 +1100

    Update dfrobot_ecpro.h

--- dfrobot_ecpro.h
+++ dfrobot_ecpro.h
@@ -2,25 +2,33 @@
 
 #include "esphome.h"
 #include "DFRobot_ECPRO.h"
+#include "esphome/components/ads1115/ads1115.h"
 
-class DFRobotECProSensor : public PollingComponent, public Sensor {
+namespace esphome {
+namespace dfrobot_ecpro {
+
+class DFRobotECProSensor : public sensor::Sensor, public PollingComponent {
  public:
-  DFRobot_ECPRO ecpro;
-
-  DFRobotECProSensor() : PollingComponent(15000) {}
+  void set_ads1115(ads1115::ADS1115Component *ads1115) { ads1115_ = ads1115; }
+  void set_multiplexer(ads1115::ADS1115Multiplexer multiplexer) { multiplexer_ = multiplexer; }
+  void set_gain(ads1115::ADS1115Gain gain) { gain_ = gain; }
 
   void setup() override {
-    // Initialize the sensor
-    ecpro.begin();
+    ecpro_.begin();
   }
 
   void update() override {
-    // Read the sensor data and publish it
-    float ec = ecpro.readEC();
-    if (!isnan(ec)) {
-      publish_state(ec);
-    } else {
-      ESP_LOGW("custom", "Failed to read EC value");
-    }
+    float voltage = ads1115_->request_measurement(multiplexer_, gain_);
+    float ec = ecpro_.readEC(voltage);
+    publish_state(ec);
   }
+
+ protected:
+  DFRobot_ECPRO ecpro_;
+  ads1115::ADS1115Component *ads1115_;
+  ads1115::ADS1115Multiplexer multiplexer_;
+  ads1115::ADS1115Gain gain_;
 };
+
+}  // namespace dfrobot_ecpro
+}  // namespace esphome
