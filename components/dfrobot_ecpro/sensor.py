f36a3c07c9339f5e5837fd230278385d658dd25e
travquinn <53845837+travquinn@users.noreply.github.com>
2024-11-12 18:41:04 +1100

    Update sensor.py

--- components/dfrobot_ecpro/sensor.py
+++ components/dfrobot_ecpro/sensor.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor
+from esphome.components import sensor, ads1115
 from esphome.const import CONF_ID, UNIT_MICROSIEMENS_PER_CENTIMETER, ICON_FLASH
 
 DEPENDENCIES = ['ads1115']
@@ -13,28 +13,6 @@ CONF_ADS1115_MULTIPLEXER = 'ads1115_multiplexer'
 CONF_ADS1115_GAIN = 'ads1115_gain'
 CONF_TEMPERATURE = 'temperature'
 
-# Define the multiplexer options
-ADS1115_MULTIPLEXER_OPTIONS = {
-    "A0_GND": 0b100,
-    "A1_GND": 0b101,
-    "A2_GND": 0b110,
-    "A3_GND": 0b111,
-    "A0_A1": 0b000,
-    "A0_A3": 0b001,
-    "A1_A3": 0b010,
-    "A2_A3": 0b011,
-}
-
-# Define the gain options
-ADS1115_GAIN_OPTIONS = {
-    "6.144V": 0b000,
-    "4.096V": 0b001,
-    "2.048V": 0b010,
-    "1.024V": 0b011,
-    "0.512V": 0b100,
-    "0.256V": 0b101,
-}
-
 CONFIG_SCHEMA = sensor.sensor_schema(
     DFRobotECProSensor,
     unit_of_measurement=UNIT_MICROSIEMENS_PER_CENTIMETER,
@@ -42,9 +20,9 @@ CONFIG_SCHEMA = sensor.sensor_schema(
     accuracy_decimals=2
 ).extend({
     cv.GenerateID(): cv.declare_id(DFRobotECProSensor),
-    cv.Required(CONF_ADS1115_ID): cv.use_id(cg.Component),
-    cv.Required(CONF_ADS1115_MULTIPLEXER): cv.enum(ADS1115_MULTIPLEXER_OPTIONS, upper=True),
-    cv.Required(CONF_ADS1115_GAIN): cv.enum(ADS1115_GAIN_OPTIONS, float=True),
+    cv.Required(CONF_ADS1115_ID): cv.use_id(ads1115.ADS1115Component),
+    cv.Required(CONF_ADS1115_MULTIPLEXER): cv.enum(ads1115.ADS1115_MULTIPLEXER_OPTIONS, upper=True),
+    cv.Required(CONF_ADS1115_GAIN): cv.enum(ads1115.ADS1115_GAIN_OPTIONS, float=True),
     cv.Optional(CONF_TEMPERATURE, default=25.0): cv.float_,
 })
 
