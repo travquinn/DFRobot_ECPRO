22d3d92afb9610224933362fc14fb2945994cbb8
travquinn <53845837+travquinn@users.noreply.github.com>
2024-11-12 18:40:47 +1100

    Update dfrobot_ecpro.h

--- components/dfrobot_ecpro/dfrobot_ecpro.h
+++ components/dfrobot_ecpro/dfrobot_ecpro.h
@@ -1,27 +1,26 @@
 #pragma once
 
-#include "esphome.h"
-#include "DFRobot_ECPRO.h"
+#include "esphome/core/component.h"
+#include "esphome/components/sensor/sensor.h"
 #include "esphome/components/ads1115/ads1115.h"
+#include "DFRobot_ECPRO.h"
 
 namespace esphome {
 namespace dfrobot_ecpro {
 
 class DFRobotECProSensor : public sensor::Sensor, public PollingComponent {
  public:
-  DFRobotECProSensor() : PollingComponent(15000) {}
-
-  void set_ads1115(ads1115::ADS1115Component *ads1115);
-  void set_ads1115_multiplexer(ads1115::ADS1115Multiplexer multiplexer);
-  void set_ads1115_gain(ads1115::ADS1115Gain gain);
-  void set_temperature(float temperature);
+  void set_ads1115(ads1115::ADS1115Component *ads1115) { ads1115_ = ads1115; }
+  void set_ads1115_multiplexer(ads1115::ADS1115Multiplexer multiplexer) { multiplexer_ = multiplexer; }
+  void set_ads1115_gain(ads1115::ADS1115Gain gain) { gain_ = gain; }
+  void set_temperature(float temperature) { temperature_ = temperature; }
 
   void setup() override {
     ecpro_ = new DFRobot_ECPRO();
   }
 
   void update() override {
-    float voltage = ads1115_->request_measurement(multiplexer_, gain_, ADS1115_RESOLUTION_16_BIT);
+    float voltage = ads1115_->request_measurement(multiplexer_, gain_);
     float ec = ecpro_->getEC_us_cm(voltage, temperature_);
     publish_state(ec);
   }
@@ -31,7 +30,7 @@ class DFRobotECProSensor : public sensor::Sensor, public PollingComponent {
   ads1115::ADS1115Component *ads1115_;
   ads1115::ADS1115Multiplexer multiplexer_;
   ads1115::ADS1115Gain gain_;
-  float temperature_ = 25.0; // Default temperature if not set
+  float temperature_ = 25.0;
 };
 
 }  // namespace dfrobot_ecpro
