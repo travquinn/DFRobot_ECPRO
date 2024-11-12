158930dda98840f6fe732359ff1b97adf1608933
travquinn <53845837+travquinn@users.noreply.github.com>
2024-11-12 18:40:26 +1100

    Update __init__.py

--- components/dfrobot_ecpro/__init__.py
+++ components/dfrobot_ecpro/__init__.py
@@ -1,1 +1,20 @@
-from .sensor import DFRobotECProSensor
+from esphome.const import CONF_ID
+from esphome import automation
+from esphome.components import sensor
+import esphome.config_validation as cv
+import esphome.codegen as cg
+
+DEPENDENCIES = ['ads1115']
+AUTO_LOAD = ['sensor']
+
+dfrobot_ecpro_ns = cg.esphome_ns.namespace('dfrobot_ecpro')
+DFRobotECProSensor = dfrobot_ecpro_ns.class_('DFRobotECProSensor', sensor.Sensor, cg.PollingComponent)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(DFRobotECProSensor),
+}).extend(cv.COMPONENT_SCHEMA)
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
+    yield sensor.register_sensor(var, config)
